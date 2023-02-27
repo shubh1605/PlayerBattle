9973b681a4ee1a821fe68172d43d240654bb61a8
shubh1605 <shubhgangar111@gmail.com>
2023-02-27 13:17:57 +0000

    urls changes

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,12 @@ urlpatterns = [
     path('profile/<id>/', profile, name='profile'),
     path('create-team/', create_team, name='create-team'),
     path('custom-admin/',admin_func, name='admin-func'),
-    path('admin/startmatch/', start_match, name='start-match'),
-    path('admin/endmatch/', end_match, name='end-match'),
-    path('admin/allotbonuspoints/', allot_bonus_points, name='allot-bonus-points'),
-    path('admin/predictresults/<id>/', predict_results, name='predict-results'),
-    path('admin/startdailymatchprediction/', start_daily_match_prediction, name='start-daily-match-prediction'),
+    path('custom-admin/startmatch/', start_match, name='start-match'),
+    path('custom-admin/endmatch/', end_match, name='end-match'),
+    path('custom-admin/allotbonuspoints/', allot_bonus_points, name='allot-bonus-points'),
+    path('custom-admin/predictresults/<id>/', predict_results, name='predict-results'),
+    path('custom-admin/startdailymatchprediction/', start_daily_match_prediction, name='start-daily-match-prediction'),
     path('edit-captain/',edit_captain, name='edit-captain'),
     path('edit-vice-captain/',edit_vice_captain, name='edit-vice-captain'),
     path('search-user/',search_user, name='search-user'),
-] 
+]
